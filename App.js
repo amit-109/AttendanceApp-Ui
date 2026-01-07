f8a6826897091db6a0eddb348f2f3cbc7338e53a
amit-rai1 <amitrai8489@gmail.com>
2026-01-07 11:27:49 +0530

    text color for drk mode

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { AppRegistry, LogBox } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { AppRegistry, LogBox, useColorScheme } from 'react-native';
+import { MD3DarkTheme, MD3LightTheme, PaperProvider, adaptNavigationTheme } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
 import { AuthProvider } from './src/contexts/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
@@ -10,11 +10,38 @@ LogBox.ignoreLogs([
   'In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.'
 ]);
 
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: undefined,
+  reactNavigationDark: undefined,
+});
+
+const CombinedDefaultTheme = {
+  ...MD3LightTheme,
+  ...LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    ...LightTheme.colors,
+  },
+};
+
+const CombinedDarkTheme = {
+  ...MD3DarkTheme,
+  ...DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    ...DarkTheme.colors,
+  },
+};
+
 function App() {
+  const colorScheme = useColorScheme();
+
+  const paperTheme = colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
-    <PaperProvider>
+    <PaperProvider theme={paperTheme}>
       <AuthProvider>
-        <NavigationContainer>
+        <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : LightTheme}>
           <AppNavigator />
           <Toast />
         </NavigationContainer>
